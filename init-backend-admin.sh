1b157b10ec6396493f93e0adb7a79c08b622f9c4
Vital Ozierski <vital@ozerich.com>
2020-05-17 15:23:14 +0300

    uncomment debug code

--- init-backend-admin.sh
+++ init-backend-admin.sh
@@ -1,9 +1,9 @@
 git clone git@github.com:ozitag/tager-admin.git admin
 git clone git@github.com:ozitag/tager-backend.git backend
 
-#rm -rf admin/.git
-#rm -rf backend/.git
-#rm -rf backend/docker
+rm -rf admin/.git
+rm -rf backend/.git
+rm -rf backend/docker
 
 mv projects/backend-admin/* .
 
