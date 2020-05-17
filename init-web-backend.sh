250238a31e9a8069688fd0813b8a36a18939688f
Vital Ozierski <vital@ozerich.com>
2020-05-17 23:50:09 +0300

    init web-backend shell script

---
+++ init-web-backend.sh
@@ -0,0 +1,11 @@
+mv projects/web-backend/* .
+mv projects/web-backend/.env.example .env.example
+cp .env.example .env
+
+./scripts/web.sh
+./scripts/backend.sh
+./scripts/cleanup.sh
+
+docker-compose -f docker-compose.local.yml --project-name tager up --build --force-recreate -d
+
+open http://localhost:8000
