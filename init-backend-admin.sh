d440f71cfe6bb4d52831343e2788b55fc13c80bb
Vital Ozierski <vital@ozerich.com>
2020-05-17 15:20:25 +0300

    init commit

---
+++ init-backend-admin.sh
@@ -0,0 +1,11 @@
+git clone git@github.com:ozitag/tager-admin.git admin
+git clone git@github.com:ozitag/tager-backend.git backend
+
+#rm -rf admin/.git
+#rm -rf backend/.git
+#rm -rf backend/docker
+
+mv projects/backend-admin/* .
+
+chmod +x ./scripts/cleanup.sh
+./scripts/cleanup.sh
---
+++ scripts/cleanup.sh
@@ -0,0 +1,6 @@
+rm -rf projects/
+rm -rf ./*.sh
+rm -rf .git
+git init
+git add -A .
+git commit -m 'init commit'
