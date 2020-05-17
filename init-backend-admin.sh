6f785b741ab95414e4016891426cd8b99b3453c8
Vital Ozierski <vital@ozerich.com>
2020-05-17 15:28:59 +0300

    move admin / backend scripts to external files

--- init-backend-admin.sh
+++ init-backend-admin.sh
@@ -1,11 +1,6 @@
-git clone git@github.com:ozitag/tager-admin.git admin
-git clone git@github.com:ozitag/tager-backend.git backend
-
-rm -rf admin/.git
-rm -rf backend/.git
-rm -rf backend/docker
+./scripts/admin.sh
+./scripts/backend.sh
 
 mv projects/backend-admin/* .
 
-chmod +x ./scripts/cleanup.sh
 ./scripts/cleanup.sh
--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,6 +1,8 @@
 rm -rf projects/
+rm -rf scripts/
 rm -rf ./*.sh
 rm -rf .git
+rm -rf README.md
 git init
 git add -A .
 git commit -m 'init commit'
