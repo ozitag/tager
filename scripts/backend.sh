5241933b7636bff38757c8ac8b4a6cf8fee9903d
Vital Ozierski <vital@ozerich.com>
2020-05-17 23:21:57 +0300

    fix backend docker configs

--- scripts/backend.sh
+++ scripts/backend.sh
@@ -4,3 +4,6 @@ cp -r backend/docker docker/backend
 rm -rf backend
 mv _backend backend
 cp backend/.env.example backend/.env
+
+sed -i -e 's/\s\/src/\s/backend/g' docker/web/Dockerfile
+sed -i -e 's/\s\/docker/\s/docker/backend/g' docker/web/Dockerfile
