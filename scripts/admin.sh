145a337702d7e9fe42346e62b72c1f31f64262d8
Vital Ozierski <vital@ozerich.com>
2020-05-19 13:57:04 +0300

    improve import assets for admin

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -2,3 +2,8 @@ git clone git@github.com:ozitag/tager-admin.git admin
 rm -rf admin/.git
 cp -r admin/docker docker/admin
 cp -r assets docker/admin-auth/assets
+
+cp assets/favicon/* admin/public/favicon
+cp assets/favicon/favicon.ico admin/public/favicon.ico
+cp assets/logo.svg admin/public/logo.svg
+cp assets/logo.png admin/public/logo.png
--- scripts/web.sh
+++ scripts/web.sh
@@ -5,5 +5,6 @@ cp web/Dockerfile docker/web/Dockerfile
 cp web/.env.example web/.env
 
 cp assets/favicon/* web/public/favicon
+cp assets/favicon/favicon.ico web/public/favicon.ico
 cp assets/logo.svg web/public/logo.svg
 cp assets/logo.png web/public/logo.png
