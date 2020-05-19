862f8103a31c39cbbca9b2df9556700b5872f120
Vital Ozierski <vital@ozerich.com>
2020-05-19 18:56:47 +0300

    fix build for admin

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -1,6 +1,6 @@
 git clone git@github.com:ozitag/tager-admin.git admin
 rm -rf admin/.git
-cp -r admin/docker docker/admin
+mv admin/docker docker/admin
 cp -r assets docker/admin-auth/assets
 
 cp assets/favicon/* admin/public/favicon
