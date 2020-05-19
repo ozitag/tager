2a11e86c5863ec238bd153afed61e095a4ee4ba1
Vital Ozierski <vital@ozerich.com>
2020-05-19 15:24:57 +0300

    replace .local with .dev

--- init-backend-admin.sh
+++ init-backend-admin.sh
@@ -6,6 +6,6 @@ cp .env.example .env
 ./scripts/backend.sh
 ./scripts/cleanup.sh
 
-docker-compose -f docker-compose.local.yml --project-name tager up --build --force-recreate -d
+docker-compose -f docker-compose.dev.yml --project-name tager up --build --force-recreate -d
 
 open http://localhost:8000
--- projects/web-backend/README.md
+++ projects/web-backend/README.md
@@ -1,2 +1,2 @@
 For local development:
-`docker-compose -f docker-compose.local.yml --project-name tager up --build --force-recreate -d`
+`docker-compose -f docker-compose.dev.yml --project-name tager up --build --force-recreate -d`
