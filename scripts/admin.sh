6bfb0d1d900941a5b18119c4e173de96f7907344
Vital Ozierski <vital@ozerich.com>
2020-05-20 15:08:15 +0300

    fix admin bash script

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -10,5 +10,6 @@ cp assets/logo.png admin/public/logo.png
 
 sed -i -e 's/COPY \.\/p/COPY \.\/admin\/p/g' docker/admin/Dockerfile
 sed -i -e 's/COPY \.\/y/COPY \.\/admin\/y/g' docker/admin/Dockerfile
+sed -i -e 's/COPY \.\/\s/COPY \.\/admin\/ /g' docker/admin/Dockerfile
 sed -i -e 's/COPY \.\/docker/COPY \.\/docker\/admin/g' docker/admin/Dockerfile
 rm docker/admin/Dockerfile-e
