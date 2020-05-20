43123ea51f702515f4024b840488efa97ef09287
Vital Ozierski <vital@ozerich.com>
2020-05-20 15:31:58 +0300

    fix 'cp -r' to 'cp -R'

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -1,7 +1,7 @@
 git clone git@github.com:ozitag/tager-admin.git admin
 rm -rf admin/.git
 mv admin/docker docker/admin
-cp -r assets docker/admin-auth/assets
+cp -R assets docker/admin-auth/assets
 
 cp assets/favicon/* admin/public/favicon
 cp assets/favicon/favicon.ico admin/public/favicon.ico
--- scripts/backend.sh
+++ scripts/backend.sh
@@ -1,6 +1,6 @@
 git clone git@github.com:ozitag/tager-backend.git backend
 mv backend/src _backend
-cp -r backend/docker docker/backend
+cp -R backend/docker docker/backend
 rm -rf backend
 mv _backend backend
 cp backend/.env.example backend/.env
