d2dc990010063ee52356f77fe485761b30469813
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-06 10:11:37 +0300

    improve cleanup script

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -1,8 +1,9 @@
 mkdir .tager
-mv scripts .tager/scripts
-mv projects .tager/projects
 mv assets .tager/assets
 mv config.json .tager/config.json
+cp scripts/admin-config.js .tager/scripts/admin-config.js
+rm -rf scripts
+rm -rf projects
 rm -rf ./init*.sh
 rm -rf .git
 git init
