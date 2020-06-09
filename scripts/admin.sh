44c86bacbd9f2ac543ddbca7afdd515b20dcb333
Vital Ozierski <v.ozierski@ozitag.com>
2020-06-09 18:02:53 +0300

    tager rebuild admin

--- scripts/admin.sh
+++ scripts/admin.sh
@@ -17,6 +17,7 @@ sed -i -e 's/COPY \.\/p/COPY \.\/admin\/p/g' docker/admin/Dockerfile
 sed -i -e 's/COPY \.\/y/COPY \.\/admin\/y/g' docker/admin/Dockerfile
 sed -i -e 's/COPY \.\/ /COPY \.\/admin\/ /g' docker/admin/Dockerfile
 sed -i -e 's/COPY \.\/docker/COPY \.\/docker\/admin/g' docker/admin/Dockerfile
+sed -i -e 's/# COPY \.\/app\.json/COPY .\/app.json/g' docker/admin/Dockerfile
 rm docker/admin/Dockerfile-e
 
 sed -i -e 's/\/favicon/\/admin\/favicon/g' admin/public/favicon/browserconfig.xml
