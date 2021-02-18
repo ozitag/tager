60f0fa6dcc70235057f2d151e980d565910f732d
Vital Ozierski <v.ozierski@ozitag.com>
2021-02-18 21:02:03 +0300

    add 503 errors

--- project-scripts/deploy-develop.sh
+++ project-scripts/deploy-develop.sh
@@ -1,10 +1,8 @@
 cd /srv/PROJECT_NAME
 
-#sudo chown www-data:www-data -R .
-
-sudo -u www-data git fetch --all
-sudo -u www-data git reset --hard origin/develop
-sudo -u www-data git clean -f -d
+git fetch --all
+git reset --hard origin/develop
+git clean -f -d
 
 sudo cp .env.development .env
 sudo cp web/env/.env.development web/.env
@@ -13,4 +11,8 @@ sudo cp backend/.env.development backend/.env
 
 sudo cp scripts/nginx-develop.conf /etc/nginx/sites-available/PROJECT_NAME
 
-sudo docker-compose -f docker-compose.dev.yml --project-name PROJECT_NAME up --build --force-recreate -d
+COMPOSE_HTTP_TIMEOUT=200 sudo docker-compose -f docker-compose.dev.yml --project-name PROJECT_NAME build
+
+touch .maintenance
+COMPOSE_HTTP_TIMEOUT=200 sudo docker-compose -f docker-compose.dev.yml --project-name PROJECT_NAME up -d --force-recreate
+rm .maintenance
--- project-scripts/deploy-master.sh
+++ project-scripts/deploy-master.sh
@@ -11,4 +11,8 @@ sudo cp web/env/.env.production web/.env
 sudo cp admin/env/.env.production admin/.env
 sudo cp backend/.env.production backend/.env
 
-sudo docker-compose -f docker-compose.yml --project-name PROJECT_NAME up --build --force-recreate -d
+COMPOSE_HTTP_TIMEOUT=200 sudo docker-compose -f docker-compose.yml --project-name PROJECT_NAME build
+
+touch .maintenance
+COMPOSE_HTTP_TIMEOUT=200 sudo docker-compose -f docker-compose.yml --project-name PROJECT_NAME up -d --force-recreate
+rm .maintenance
